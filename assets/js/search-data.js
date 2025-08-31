36152a0b8ffca39e4707917de23479d0cbeee231
arshiaafzal <68875155+arshiaafzal@users.noreply.github.com>
2025-08-31 10:03:09 +0000

    Deploying to gh-pages from @ arshiaafzal/SSM_Story@d437eecb91b75f88264d0754d4a6cb6b004a3bf3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,6 +24,17 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
+          window.location.href = "/SSM_Story/2025/lion-part3-chunk/";
+        
+      },
+    },{id: "post-story-of-linear-time-sequence-modeling",
+      
+        title: "Story of Linear Time Sequence Modeling 📚",
+      
+      description: "Summary of Linear Transformers",
+      section: "Posts",
+      handler: () => {
+        
           window.location.href = "/SSM_Story/2025/lion-part2-theory/";
         
       },
@@ -47,17 +58,6 @@ ninja.data = [{
       handler: () => {
         
           window.location.href = "/SSM_Story/2025/lion-part4-results/";
-        
-      },
-    },{id: "post-lion-part-iii-chunkwise-parallel-from-of-lion",
-      
-        title: "LION 🦁 Part III - Chunkwise Parallel from of LION",
-      
-      description: "Explaining LION-Chunk for Balancing Memory-Speed Tradeoffs During Inference",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/SSM_Story/2025/lion-part3-chunk/";
         
       },
     },{id: "news-a-simple-inline-announcement",
