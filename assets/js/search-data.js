45b4c156165d36e27db20f05d00951d5f39efcc5
arshiaafzal <68875155+arshiaafzal@users.noreply.github.com>
2025-08-31 09:34:07 +0000

    Deploying to gh-pages from @ arshiaafzal/SSM_Story@a7d2e64ab3658cb8e1d9f8ec95ea3fcf5faf068d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -7,14 +7,14 @@ ninja.data = [{
     title: "",
     section: "Navigation",
     handler: () => {
-      window.location.href = "/";
+      window.location.href = "/SSM_Story/";
     },
   },{id: "nav-github",
           title: "github",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/https:/arshiaafzal.github.io/SSM_Story/";
+            window.location.href = "/SSM_Story/https:/arshiaafzal.github.io/SSM_Story/";
           },
         },{id: "post-lion-part-iv-results",
       
@@ -24,7 +24,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/2025/lion-part4-results/";
+          window.location.href = "/SSM_Story/2025/lion-part4-results/";
         
       },
     },{id: "post-lion-part-iii-chunkwise-parallel-from-of-lion",
@@ -35,7 +35,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/2025/lion-part3-chunk/";
+          window.location.href = "/SSM_Story/2025/lion-part3-chunk/";
         
       },
     },{id: "post-lion-part-ii-bi-directional-rnn",
@@ -46,7 +46,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/2025/lion-part2-theory/";
+          window.location.href = "/SSM_Story/2025/lion-part2-theory/";
         
       },
     },{id: "post-lion-part-i-full-linear-attention",
@@ -57,7 +57,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/2025/lion-part1-model/";
+          window.location.href = "/SSM_Story/2025/lion-part1-model/";
         
       },
     },{id: "news-a-simple-inline-announcement",
@@ -67,7 +67,7 @@ ninja.data = [{
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
+              window.location.href = "/SSM_Story/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
@@ -75,45 +75,45 @@ ninja.data = [{
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
+              window.location.href = "/SSM_Story/projects/1_project/";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
+              window.location.href = "/SSM_Story/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
+              window.location.href = "/SSM_Story/projects/3_project/";
             },},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
+              window.location.href = "/SSM_Story/projects/4_project/";
             },},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
+              window.location.href = "/SSM_Story/projects/5_project/";
             },},{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
+              window.location.href = "/SSM_Story/projects/6_project/";
             },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
+              window.location.href = "/SSM_Story/projects/7_project/";
             },},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
+              window.location.href = "/SSM_Story/projects/8_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/SSM_Story/projects/9_project/";
             },},];
